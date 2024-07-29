71b1c95af2ab1e0995e1878d5e61cc14aa081811
Tomás <arrativeltomas@gmail.com>
2024-07-29 14:57:59 -0300

    Initial files and some routes created

---
+++ routes/expense.js
@@ -0,0 +1,20 @@
+// require express and it's router component
+const express = require("express");
+
+const router = express.Router();
+
+// require the middlewares and callback functions from the controller directory
+const { create, expenseById, read } = require("../controllers");
+// Create POST route to create an expense
+router.post("/expense/create", create);
+
+// Create GET route to read an expense
+router.get("/expense/:id", expenseById, read);
+
+// Create PUT route to update an expense
+
+// Create DELETE route to remove an expense
+
+// Create GET route to read a list of expenses
+
+module.exports = router;
